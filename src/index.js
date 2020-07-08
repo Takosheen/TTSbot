c55ab0772c03637218cdf622a31e3e25fd12a367
Terence Au <60273779+Takosheen@users.noreply.github.com>
2020-07-08 18:08:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,27 @@ client.once('ready', () => {
 
 client.login('NzI4NzExMDE2MTc0NTgzODA4.Xv-ZRA.WXUp-lzvg5lhbS-JuKPdaitBuag');
 
-client.on("channelCreate", (channel) => { //method looks for channel 
+client.on("channelCreate", (channel) => { //method looks for channel
+    
     var message = new Discord.Message(client,null,channel);
+    
+    // extract text into a separate variable
     message.channel.send('text').then((sentmessage)=>{ //text would be what to prompt user with
-        sentmessage.react('😀'); // reaction to message for role
+        const filter = (reaction, user) => {
+            
+            // set up emojis to be filtered for categories
+          return reaction.emoji.name === '🙂'
+        }
+    
+        var collector = sentmessage.createReactionCollector(filter)
+        
+        collector.on('collect', (reaction, user) => {
+            // Possibly make this into a switch statement, maybe later
+            if(reaction.emoji.name === '🙂')
+               
+        })
+        sentmessage.react('🙂'); // reaction to message for role
+        //add emojis for other reactions
     });
 });
+
